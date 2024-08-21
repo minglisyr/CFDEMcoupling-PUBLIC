89777eab7c90d90a1fcc5c7ef4664cddf29cd6df
minglisyr <80295234+minglisyr@users.noreply.github.com>
2024-08-20 20:46:38 -0600

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -17,11 +17,14 @@ cd OpenFOAM
 git clone https://github.com/OpenFOAM/OpenFOAM-5.x.git
 git clone https://github.com/OpenFOAM/ThirdParty-5.x.git
 cd OpenFOAM-5.x
+git checkout 538044ac05c4672b37c7df607dca1116fa88df88
+
 sudo apt-get install build-essential flex bison cmake zlib1g-dev libboost-system-dev libboost-thread-dev libopenmpi-dev openmpi-bin gnuplot libreadline-dev libncurses-dev libxt-dev libscotch-dev libptscotch-dev paraview
+#sudo apt install python3-pip
 #sudo apt install vtk
 #sudo apt install libvtk9-dev
-sudo apt install python3-pip
 #pip3 install numpy
+
 sudo apt update
 sudo apt upgrade
 echo -e "\nexport WM_NCOMPPROCS=4\nsource \$HOME/OpenFOAM/OpenFOAM-5.x/etc/bashrc" >> ~/.bashrc
@@ -43,7 +46,7 @@ export CFDEM_bashrc=$CFDEM_PROJECT_DIR/src/lagrangian/cfdemParticle/etc/bashrc
 export CFDEM_LIGGGHTS_SRC_DIR=$HOME/LIGGGHTS/LIGGGHTS-PUBLIC/src
 export CFDEM_LIGGGHTS_MAKEFILE_NAME=auto
 export CFDEM_LPP_DIR=$HOME/LIGGGHTS/lpp/src
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/build/lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/build/lib
 . $CFDEM_bashrc
 #================================================#
 EOF
